cd5c418f1ede62a0770d9b17d6602df188193d35
18016165 <18016165@myrp.edu.sg>
2020-05-04 16:06:56 +0800

    Second way to sort data other than hardcode

--- app/src/main/java/sg/edu/rp/c346/ps_sgholiday/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/ps_sgholiday/SecondActivity.java
@@ -32,21 +32,38 @@ public class SecondActivity extends AppCompatActivity {
 
         holiday1 = new ArrayList<Holiday>();
         holiday2 = new ArrayList<Holiday>();
-        holiday1.add(new Holiday("Secular", "New Year's Day", "1 Jan 2017", "newyear"));
-        holiday1.add(new Holiday("Secular", "Labour Day", "1 May 2017", "labourday"));
-        holiday2.add(new Holiday("Ethnic and Religion", "Chinese New Year", "28 - 29 Jan 2017", "cny"));
-        holiday2.add(new Holiday("Ethnic and Religion", "Good Friday", "14 April 2017", "goodfriday"));
+
+        Holiday ny = new Holiday("Secular", "New Year's Day", "1 Jan 2017", "newyear");
+        holiday1.add(ny);
+        Holiday ld = new Holiday("Secular", "Labour Day", "1 May 2017", "labourday");
+        holiday1.add(ld);
+        Holiday cny = new Holiday("Ethnic & Religion", "Chinese New Year", "28 - 29 Jan 2017", "cny");
+        holiday1.add(cny);
+        Holiday gf = new Holiday("Ethnic & Religion", "Good Friday", "14 April 2017", "goodfriday");
+        holiday1.add(gf);
+
+        //holiday1.add(new Holiday("Secular", "New Year's Day", "1 Jan 2017", "newyear"));
+        //holiday1.add(new Holiday("Secular", "Labour Day", "1 May 2017", "labourday"));
+        //holiday2.add(new Holiday("Ethnic and Religion", "Chinese New Year", "28 - 29 Jan 2017", "cny"));
+        //holiday2.add(new Holiday("Ethnic and Religion", "Good Friday", "14 April 2017", "goodfriday"));
 
         Intent i = getIntent();
         category = i.getStringExtra("Holi Type");
         tvHoliCat.setText(category);
 
-        if(category.equals("Secular")){
-            aa = new HolidayAdapter(this, R.layout.row, holiday1);
-        }else if(category.equals("Ethnic & Religion")){
-            aa = new HolidayAdapter(this, R.layout.row, holiday2);
+        for (int c = 0; c < holiday1.size(); c++){
+            if(holiday1.get(c).getCategory().equals(category)){
+                holiday2.add(holiday1.get(c));
+            }
         }
 
+        //if(category.equals("Secular")){
+        //    aa = new HolidayAdapter(this, R.layout.row, holiday1);
+        //}else if(category.equals("Ethnic & Religion")){
+        //    aa = new HolidayAdapter(this, R.layout.row, holiday2);
+        //}
+
+        aa = new HolidayAdapter(this, R.layout.row, holiday2);
         lvHoli.setAdapter(aa);
 
         lvHoli.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -54,15 +71,20 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                if(category.equals("Secular")){
-                    Holiday selectedHoliday = holiday1.get(position);
+                //if(category.equals("Secular")){
+                //    Holiday selectedHoliday = holiday1.get(position);
 
-                    Toast.makeText(SecondActivity.this, selectedHoliday.getHoliName() + ", Date: " + selectedHoliday.getDate(), Toast.LENGTH_LONG).show();
-                }else{
-                    Holiday selectedHoliday = holiday2.get(position);
+                //    Toast.makeText(SecondActivity.this, selectedHoliday.getHoliName() + ", Date: " + selectedHoliday.getDate(), Toast.LENGTH_LONG).show();
+                //}else{
+                //    Holiday selectedHoliday = holiday2.get(position);
 
-                    Toast.makeText(SecondActivity.this, selectedHoliday.getHoliName() + ", Date: " + selectedHoliday.getDate(), Toast.LENGTH_LONG).show();
-                }
+                //    Toast.makeText(SecondActivity.this, selectedHoliday.getHoliName() + ", Date: " + selectedHoliday.getDate(), Toast.LENGTH_LONG).show();
+
+                //}
+
+                Holiday selectedHoliday = holiday2.get(position);
+
+                Toast.makeText(SecondActivity.this, selectedHoliday.getHoliName() + ", Date: " + selectedHoliday.getDate(), Toast.LENGTH_LONG).show();
 
             }
         });
